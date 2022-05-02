39aef327dbb044d0095729491515d9c171a38241
Mahir Tazwar <mahirtaz@gmail.com>
2022-05-02 22:29:22 +0600

    development phase 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ListingController;
+use App\Models\Listing;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ListingController::class,'index']);
+
+Route::get('/listings/create',[ListingController::class,'create']);
+
+Route::post('/listings',[ListingController::class,'store']);
+
+Route::get('/listings/{listing}/edit',[ListingController::class,'edit']);
+
+Route::put('/listings/{listing}',[ListingController::class,'update']);
+
+
+Route::get('/listings/{id}',[ListingController::class,'show']);
+
+
+
+
